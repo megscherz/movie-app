00a187ea59da36bae1bc3e6618addb05b78a0652
Meghan Scherzer <meghan.scherzer@gmail.com>
2022-01-19 10:38:47 -0600

    adds movie migrations and updates controller attributes

---
+++ db/migrate/20220119160641_add_englishto_movie.rb
@@ -0,0 +1,5 @@
+class AddEnglishtoMovie < ActiveRecord::Migration[7.0]
+  def change
+    add_column :movies, :english, :string
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,22 @@
-# actor = Actor.new({ first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock" })
-# actor.save
+# Actor.create!([
+#   {first_name: "Nathan", last_name: "Fillion", known_for: "Firefly"},
+#   {first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock"},
+#   {first_name: "Kate", last_name: "Winslet", known_for: "Titanic"},
+#   {first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock"},
+#   {first_name: "Kate", last_name: "Winslet", known_for: "Titanic"},
+#   {first_name: "Jennifer", last_name: "Lawrence", known_for: "Hunger Games series"},
+#   {first_name: "George", last_name: "Clooney", known_for: "Oceans 11"},
+#   {first_name: "Tom", last_name: "Holland", known_for: "Spider-Man"},
+#   {first_name: "Kirsten", last_name: "Wiig", known_for: "Bridesmaids"},
+#   {first_name: "Melissa", last_name: "McCarthy", known_for: "Bridesmaids"},
+#   {first_name: "Paul", last_name: "Rudd", known_for: "I Love You, Man"},
+#   {first_name: "Jennifer", last_name: "Lawrence", known_for: "Silver Linings Playbook"}
+# ])
 
-# actor = Actor.new({ first_name: "Kate", last_name: "Winslet", known_for: "Titanic" })
-# actor.save
-
-# actor = Actor.new({ first_name: "Jennifer", last_name: "Lawrence", known_for: "Hunger Games series" })
-# actor.save
-
-# actor = Actor.new({ first_name: "George", last_name: "Clooney", known_for: "Oceans 11" })
-# actor.save
-
-# actor = Actor.new({ first_name: "Melissa", last_name: "McCarthy", known_for: "Bridesmaids" })
-# actor.save
-
-# actor = Actor.new({ first_name: "Paul", last_name: "Rudd", known_for: "I Love You, Man" })
-# actor.save
-
-movie = Movie.new(title: "Bird Box", year: 2018, plot: "A post-apocolyptic horror movie about an unseen presence that drives most of society to suicide. A family, however, try to make it to safety.")
-movie.save
-
-movie = Movie.new(title: "Harry Potter and the Sorcerer's Stone", year: 2001, plot: "A young boy finds out that he's a wizard and can go to the best wizarding school in the country. The movie captures his first year at school and Harry learning all about himself and his family's past.")
-movie.save
+Movie.create!([
+  { title: "Bridesmaids", year: 2011, plot: "A competition between the maid of honor and a bridesmaid threathen to upend the lift of an out-of-work pastry chef.", director: "Paul Feig", english: true },
+  { title: "Horrible Bosses", year: 2011, plot: "Three friends plot to kill their oppressive employers and of course, chaos ensues.", director: "Seth Gordon", english: true },
+  { title: "Bird Box", year: 2018, plot: "A post-apocolyptic horror movie about an unseen presence that drives most of society to suicide. A family, however, try to make it to safety.", director: "Sandra Bier", english: true },
+  { title: "Harry Potter and the Sorcerer's Stone", year: 2001, plot: "A young boy finds out that he's a wizard and can go to the best wizarding school in the country. The movie captures his first year at school and Harry learning all about himself and his family's past.", director: "Chris Columbus", english: true },
+  { title: "I Love You, Man", year: 2009, plot: "A man without friends needs a bestman for his wedding. He meets this strange man meet and become friends. Things are learned and in the end they are best friends.", director: "John Hamburg", english: true },
+])
