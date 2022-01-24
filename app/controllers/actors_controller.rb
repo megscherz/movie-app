1fb605384cc307d9adac5198b416fb66b2d5851c
Meghan Scherzer <meghan.scherzer@gmail.com>
2022-01-24 17:55:43 -0600

    little updates

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -11,6 +11,7 @@ class ActorsController < ApplicationController
       known_for: params[:known_for],
       gender: params[:gender],
       age: params[:age],
+      movie_id: params[:movie_id],
     )
     if actor.save
       render json: actor
@@ -26,11 +27,12 @@ class ActorsController < ApplicationController
 
   def update
     actor = Actor.find(params[:id])
-    actor.first_name = params[:first_name] || actor.first_name
-    actor.last_name = params[:last_name] || actor.last_name
-    actor.known_for = params[:known_for] || actor.known_for
-    actor.gender = params[:gender] || actor.gender
-    actor.age = params[:age] || actor.age
+    actor.first_name = params[:first_name] || actor.first_name,
+                       actor.last_name = params[:last_name] || actor.last_name,
+                       actor.known_for = params[:known_for] || actor.known_for,
+                       actor.gender = params[:gender] || actor.gender,
+                       actor.age = params[:age] || actor.age,
+    actor.movie_id = params[:movie_id] || actor.movie_id
     actor.save
     if actor.save
       render json: actor
